b481e34259d2ca3e05e18c7af2ffc6816330cf95
Vu Thien Khiem <vuthienkhiem2005@gmail.com>
2025-11-13 13:48:02 +0700

    cbi sang main

--- EmployeeManagement.BLL/Services/ActivityLogService.cs
+++ EmployeeManagement.BLL/Services/ActivityLogService.cs
@@ -4,24 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EmployeeManagement.DAL.Models;
+using EmployeeManagement.DAL.Repositories;
 
 namespace EmployeeManagement.BLL.Services
 {
     public class ActivityLogService : IActivityLogService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ActivityLogService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IEnumerable<ActivityLog> GetActivityLogs(DateTime startDate, DateTime endDate)
         {
-            throw new NotImplementedException();
+            return _unitOfWork.ActivityLogRepository
+                .Find(log => log.Timestamp >= startDate && log.Timestamp <= endDate)
+                .ToList();
         }
 
         public IEnumerable<ActivityLog> GetLogsByAdmin(int adminId)
         {
-            throw new NotImplementedException();
+            return _unitOfWork.ActivityLogRepository
+                .Find(log => log.AccountId == adminId)
+                .ToList();
+
         }
 
         public void LogActivity(int adminId, string actionType, string details)
         {
-            throw new NotImplementedException();
+            _unitOfWork.ActivityLogRepository.Add(new ActivityLog
+            {
+                AccountId = adminId,
+                ActionType = actionType,
+                Details = details,
+                Timestamp = DateTime.UtcNow
+            });
         }
     }
 }
